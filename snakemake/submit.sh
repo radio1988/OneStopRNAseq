abfabb5d68daa2a3cd607778305fd76c470173db
radio1988 <liruiradiant@gmail.com>
2020-05-21 08:19:19 -0400

    polished

--- snakemake/submit.sh
+++ snakemake/submit.sh
@@ -3,3 +3,7 @@ nohup snakemake --use-conda -k  --jobs 999 --latency-wait 604800 \
 
 nohup snakemake -k --jobs 999 --latency-wait 1800 \
 --cluster 'bsub -q short -o lsf.log -R "rusage[mem={params.mem_mb}]" -n {threads} -R span[hosts=1] -W 4:00' && snakemake --report report.html &
+
+snakemake --export-cwl workflow.cwl
+
+mv DESeq2/ mapped_reads/ sorted_reads/ feature_count/ bigWig/ bam_qc/ fastqc/ trash
