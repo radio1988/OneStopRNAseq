09ee56b71c8beeadc3d292ceb84f2ccc2c856b9a
radio1988 <liruiradiant@gmail.com>
2020-05-21 07:54:00 -0400

    works on mac and hpc

--- snakemake/submit.sh
+++ snakemake/submit.sh
@@ -1,5 +1,5 @@
 nohup snakemake --use-conda -k  --jobs 999 --latency-wait 604800 \
 --cluster 'bsub -q long -o lsf.log -R "rusage[mem={params.mem}]" -n {threads} -R span[hosts=1] -W 168:00' && snakemake --report report.html &
 
-nohup snakemake --use-conda -k --jobs 999 --latency-wait 14400 \
+nohup snakemake -k --jobs 999 --latency-wait 1800 \
 --cluster 'bsub -q short -o lsf.log -R "rusage[mem={params.mem_mb}]" -n {threads} -R span[hosts=1] -W 4:00' && snakemake --report report.html &
