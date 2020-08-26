54eeaefa45a374344bae51f0f547725760392a15
radio1988 <liruiradiant@gmail.com>
2020-08-26 04:54:29 -0400

    fixed bugs: countFile removed from argv; grepl bug, use group label; fix ref to name2; try plots;

--- snakemake/script/dexseq.r
+++ snakemake/script/dexseq.r
@@ -8,7 +8,7 @@ suppressPackageStartupMessages(library(WriteXLS))
 suppressPackageStartupMessages(library(BiocParallel))
 sessionInfo()
 
-BPPARAM = MulticoreParam(workers=8)
+BPPARAM = MulticoreParam(workers=8) # test
 
 readExcel <- function(fname){
   df <- readxl::read_xlsx( fname, na='NA', sheet=1)  # na term important
@@ -42,39 +42,40 @@ plotMAWrapper <- function(dxd, ourDir, name){
 	dev.off()
 }
 
-outDir <- "./DEXSeq/"
+outDir <- "./DEXSeq/" # test
 dir.create(outDir, showWarnings = FALSE)
 args = commandArgs(trailingOnly=TRUE)
 
 # Read ARGS
+# todo: remove countFile from args
 if (length(args) < 2){
     # development
     metaFile <- 'meta/meta.xlsx'  
     contrastFile <- './meta/contrast.de.xlsx'
     gffFile <- '/project/umw_mccb/OneStopRNAseq/kai/DEXSeq/gff/gencode.v34.primary_assembly.annotation.DEXSeq.gff'
     gffFile <- 'gencode.v34.primary_assembly.annotation.DEXSeq.gff'
-    countFile <- 'DEXSeq_count/N052611_Alb_Dex_count.txt DEXSeq_count/N052611_Alb_count.txt DEXSeq_count/N052611_Dex_count.txt DEXSeq_count/N052611_untreated_count.txt DEXSeq_count/N061011_Alb_Dex_count.txt DEXSeq_count/N061011_Alb_count.txt DEXSeq_count/N061011_Dex_count.txt DEXSeq_count/N061011_untreated_count.txt DEXSeq_count/N080611_Alb_Dex_count.txt DEXSeq_count/N080611_Alb_count.txt DEXSeq_count/N080611_Dex_count.txt DEXSeq_count/N080611_untreated_count.txt DEXSeq_count/N61311_Alb_Dex_count.txt DEXSeq_count/N61311_Alb_count.txt DEXSeq_count/N61311_Dex_count.txt DEXSeq_count/N61311_untreated_count.txt'
+    #countFile <- 'DEXSeq_count/N052611_Alb_Dex_count.txt DEXSeq_count/N052611_Alb_count.txt DEXSeq_count/N052611_Dex_count.txt DEXSeq_count/N052611_untreated_count.txt DEXSeq_count/N061011_Alb_Dex_count.txt DEXSeq_count/N061011_Alb_count.txt DEXSeq_count/N061011_Dex_count.txt DEXSeq_count/N061011_untreated_count.txt DEXSeq_count/N080611_Alb_Dex_count.txt DEXSeq_count/N080611_Alb_count.txt DEXSeq_count/N080611_Dex_count.txt DEXSeq_count/N080611_untreated_count.txt DEXSeq_count/N61311_Alb_Dex_count.txt DEXSeq_count/N61311_Alb_count.txt DEXSeq_count/N61311_Dex_count.txt DEXSeq_count/N61311_untreated_count.txt'
   }else{
     # production
     metaFile <- args[1]
     contrastFile <- args[2]
     gffFile <- args[3]
-    countFile <-paste( unlist(args[4:length(args)]), collapse=' ')  
+    #countFile <-paste( unlist(args[4:length(args)]), collapse=' ')  
   }
 
 
-min_count_per_exon <- 10000  # test, maybe not valid for DEXSeq if larger than 1
+
+min_count_per_exon <- 0  # test, maybe not valid for DEXSeq if larger than 1
 maxFDR <- 0.05  # todo: read config
 #minLFC <- 0.585
 
-countFile <- gsub(' +',' ',countFile) 
+#countFile <- gsub(' +',' ',countFile) 
 print(">>> Parameters: ")
 print(paste("path:", getwd()))
 paste("metaFile:", metaFile)
 paste("contrastFile:", contrastFile)
 paste("gffFile:", gffFile)
-print("countFile:")
-print(countFile)
+
 
 cat("\n\nreading metaFile:\n")
 meta.df <- readExcel(metaFile)
@@ -83,8 +84,15 @@ cat("\n\nreading contrastFile:\n")
 contrast.df <- readExcel(contrastFile)
 print(contrast.df)
 
+countFile <- paste('DEXSeq_count/', meta.df$SAMPLE_LABEL, "_count.txt", sep='')
+print("countFile:")
+print(countFile)
+if (!all(file.exists(countFile))){
+	stop("Some countFiles Can't be found!!!")
+}
+
 ## name1 should be treated group
-for (i in 1:dim(contrast.df)[2]) {
+for (i in 1:dim(contrast.df)[2]) { # test
   # parse names
   name1 <- contrast.df[1, i]
   name2 <- contrast.df[2, i]
@@ -98,59 +106,39 @@ for (i in 1:dim(contrast.df)[2]) {
   name2 <- gsub(";", ".", name2)
   name <- paste(name1, name2, sep = "_vs_")
   cat(paste("\n\n>>> for contrast", i, ":", name, "\n"))
-  
-  sample <- factor(meta.df$SAMPLE_LABEL)
-  batch <- factor(meta.df$BATCH)
-  
-  row_names     <- c()
-  condition_col <- c()
-  batch_col     <- c()
-  countFiles    <- unlist(strsplit(countFile, " "))
-  countFilesSubset <- c()
-  
-  for (i2 in 1:length(sample)) {
-    for (j in 1:length(name1s)) {
-      if (grepl(name1s[j], sample[i2], fixed=TRUE)) {
-        row_names <- c(row_names, as.character(sample[i2]))
-        condition_col <- c(condition_col, name1) # GROUP1
-        batch_col <- c(batch_col, as.character(batch[i2]))
-        countFilesSubset <- c(countFilesSubset, countFiles[i2])
-      }
-    }
-    for (l in 1:length(name2s)) {
-      if (grepl(name2s[l], sample[i2], fixed=TRUE)) {
-        row_names <- c(row_names, as.character(sample[i2]))
-        condition_col <- c(condition_col, name2) # GROUP2, check when ; in contrast GROUP labels
-        batch_col <- c(batch_col, as.character(batch[i2]))
-        countFilesSubset <- c(countFilesSubset, countFiles[i2])
-      }
-    }
-  }
-  sampleTable <- data.frame(row.names = row_names,
-                            condition = condition_col,
-                            batch     = batch_col)
+
+  sampleTable <- data.frame(row.names = meta.df$SAMPLE_LABEL,
+                            condition = meta.df$GROUP_LABEL,
+                            batch     = meta.df$BATCH)
+
+  idx <- sampleTable$condition %in% c(name1, name2)
+  sampleTableSubset <- sampleTable[idx, ]
+  countFilesSubset <- countFile[idx]
   
   # Print sampleTable:
   print('countFilesSubset:')
   print(countFilesSubset)
-  cat("\nsample table: \n")
-  print(sampleTable)
+  cat("\nsampleTableSubset: \n")
+  print(sampleTableSubset)
   cat("\n")
     
   # Read data
-  print(paste("reading:", countFilesSubset))
+  print("reading data..")
   dxd = DEXSeqDataSetFromHTSeq(
     countFilesSubset,
-    sampleData=sampleTable,
+    sampleData=sampleTableSubset,
     design= ~ sample + exon + condition:exon,
     flattenedfile=gffFile )
-  print(dxd)
+  dxd$condition <- relevel(dxd$condition, ref = name2)
 
   # Filter data (skipped to keep figures in results correct)
-  print(paste("Removing bins/exons with less than ", min_count_per_exon, "reads"))
-  dxd <- dxd[rowSums(featureCounts(dxd)) >= min_count_per_exon, ]
+  if (min_count_per_exon > 0){
+    print(paste("Removing bins/exons with less than ", min_count_per_exon, "reads"))
+    dxd <- dxd[rowSums(featureCounts(dxd)) >= min_count_per_exon, ]
+  }
+
   print(dxd)
-  print(head(geneIDs(dxd), 3))
+  #print(head(geneIDs(dxd), 3))
   print(head(featureCounts(dxd), 5))
   
   print("Estimate size factors..")
@@ -158,7 +146,7 @@ for (i in 1:dim(contrast.df)[2]) {
   
   ## if only one batch, don't apply model, otherwise DEXSeq error:
   print("Estimate Dispersion and performing statistical test..")
-  if (length(unique(batch)) == 1) {
+  if (length(unique(sampleTableSubset$BATCH)) == 1) {
   	print("No Batch Effect")
     dxd = estimateDispersions(dxd, BPPARAM=BPPARAM)
     dxd = testForDEU(dxd, BPPARAM=BPPARAM)
@@ -178,7 +166,7 @@ for (i in 1:dim(contrast.df)[2]) {
   # Summarize result:
   cat("\nSummarize result:\n")
   dxr = DEXSeqResults(dxd)
-  save.image(file=paste("contrast", i, "RData", sep='.'))
+  save.image(file=paste(outDir, "contrast", i, ".RData", sep=''))
   
   # Some statistics:
   cat(paste("\n\nnum of DE exons with FDR <", maxFDR, "\n"))
@@ -191,8 +179,9 @@ for (i in 1:dim(contrast.df)[2]) {
   ## filter out rows with no padj value, otherwise, the excel might be too huge.
   df_dxr <- data.frame(dxr)
   df_dxr <- df_dxr[!is.na(df_dxr$padj), ]
+  colnames(df_dxr) <- gsub('countData.', '', colnames(df_dxr))
   print(paste("Saving results to:", fname))
-  WriteXLS(df_dxr, row.names = F, fname)
+  try(WriteXLS(df_dxr, row.names = F, fname)) # if only nrow > 0
   
   # QC plots:
   try(plotDispEstsWrapper(dxd, outDir, name))
