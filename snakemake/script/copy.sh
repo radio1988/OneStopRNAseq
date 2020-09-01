2e568523acc65e160eba9ffc329fd9c7818ba9b5
radio1988 <liruiradiant@gmail.com>
2020-09-01 18:09:35 -0400

    organized structure

---
+++ snakemake/script/copy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rsync -aP /project/umw_mccb/OneStopRNAseq/rui/develop/OneStopRNAseq/snakemake/Snakefile .
+rsync -aP  /project/umw_mccb/OneStopRNAseq/rui/develop/OneStopRNAseq/snakemake/script .
+rsync -aP  /project/umw_mccb/OneStopRNAseq/rui/develop/OneStopRNAseq/snakemake/submit.sh .
+rsync -aP  /project/umw_mccb/OneStopRNAseq/rui/develop/OneStopRNAseq/snakemake/meta .
+ln -s /project/umw_mccb/OneStopRNAseq/rui/develop/OneStopRNAseq/snakemake/envs/
+source activate osr
+echo "you need to create config.yaml, fastq/"
