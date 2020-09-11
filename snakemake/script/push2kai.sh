2068649905be9fb38a102d6fd898a87af4b2f5eb
radio1988 <liruiradiant@gmail.com>
2020-09-10 20:31:16 -0400

    removed gsea.finished flag

--- snakemake/script/push2kai.sh
+++ snakemake/script/push2kai.sh
@@ -1,1 +1,1 @@
-rsync -aP Snakefile config.yaml envs submit.sh script meta genome fastq README.md /project/umw_mccb/OneStopRNAseq/rui/delivery/
+rsync -aP Snakefile config.yaml envs submit.sh script meta example_data fastq README.md /project/umw_mccb/OneStopRNAseq/rui/delivery/
