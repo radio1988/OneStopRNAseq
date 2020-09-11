56b79fd7d31ad297c5495e6d9d87af23bcfefbb9
radio1988 <liruiradiant@gmail.com>
2020-09-10 21:11:27 -0400

    removed meta from copy.sh

--- snakemake/script/copy.sh
+++ snakemake/script/copy.sh
@@ -2,7 +2,6 @@
 rsync -aP /project/umw_mccb/OneStopRNAseq/rui/develop/OneStopRNAseq/snakemake/Snakefile .
 rsync -aP  /project/umw_mccb/OneStopRNAseq/rui/develop/OneStopRNAseq/snakemake/script .
 rsync -aP  /project/umw_mccb/OneStopRNAseq/rui/develop/OneStopRNAseq/snakemake/submit.sh .
-rsync -aP  /project/umw_mccb/OneStopRNAseq/rui/develop/OneStopRNAseq/snakemake/meta .
 ln -s /project/umw_mccb/OneStopRNAseq/rui/develop/OneStopRNAseq/snakemake/envs/
 source activate osr
-echo "you need to create config.yaml, fastq/"
+echo "you need to create config.yaml, fastq/, meta/"
