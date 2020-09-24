c0dd8b6e067d39d5b5df312ec384d15155f8c02c
radio1988 <liruiradiant@gmail.com>
2020-09-24 16:12:41 -0400

    on the fly zipping

--- snakemake/script/zip.sh
+++ snakemake/script/zip.sh
@@ -3,4 +3,5 @@ ARRAY=(fastqc bam_qc DESeq2 DEXSeq `ls -d  rMAT*` GATK_ASEReadCounter )
 for D in "${ARRAY[@]}"
 do
 rm -f $D/$D.zip && [ -d $D ] && zip -rq  $D/$D.zip $D/ 
+#rm -f $D.zip && [ -d $D ] && zip -rq $D.zip $D/ &>> {log} # only for subfolders
 done
