3634d33557eb3e1a06f5fd17f48a7231361a0bd3
radio1988 <liruiradiant@gmail.com>
2020-12-23 16:08:35 -0500

    make_all_input_output_tracked;

--- snakemake/submit.sh
+++ snakemake/submit.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 # run with:
 # nohup bash submit.sh &
-# OR: bsub -q long -W 144:00 -R rusage[mem=8000] 'bash submit.sh'
+# OR: bsub -q long -W 144:00 -R rusage[mem=4000] 'bash submit.sh'
 
 module purge
 module load singularity/singularity-current > nohup.out   2>&1 
 source activate osr >> nohup.out  2>&1 
 
-snakemake -p -k --jobs 999 \
+
+# fast jobs with short queue
+snakemake -p -k --jobs 16 \
+--use-envmodules \
+--use-singularity \
+--use-conda  --conda-prefix "/project/umw_mccb/OneStopRNAseq/conda/" \
+--latency-wait 120 \
+--ri --restart-times 0 \
+--cluster 'bsub -q short -o lsf.log -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 4:00' >> nohup.out  2>&1 
+
+# slower jobs with long queue
+# low RAM jobs ran 3 times with triple RAM
+snakemake -j 1 --unlock
+snakemake -p -k --jobs 16 \
 --use-envmodules \
 --use-singularity \
 --use-conda  --conda-prefix "/project/umw_mccb/OneStopRNAseq/conda/" \
 --latency-wait 300 \
---ri --restart-times 1 \
---cluster 'bsub -q long -o lsf.log -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 72:00' >> nohup.out  2>&1 
+--ri --restart-times 2 \
+--cluster 'bsub -q long -o lsf.log -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 48:00' >> nohup.out  2>&1 
+
+
 # note: envmodules > singularity > conda > osr
 
-snakemake --report report.html > report.log  2>&1
+snakemake -j 1 --report report.html > report.log  2>&1
 
 # bash script/zip.sh
 
-
 # gsea compression (should be skipped)
 # [ -d 'gsea/' ] && tar cf - gsea/  | pigz -p 2 -f > gsea.tar.gz && mv gsea.tar.gz gsea
-
-
-
 
 ## Handy commands for development
 # bkill -r rl44w 0
