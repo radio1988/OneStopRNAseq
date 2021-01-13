040f5f6a046aacbbe57aafe8ea90d772433809a5
radio1988 <liruiradiant@gmail.com>
2021-01-12 23:51:52 -0500

    local old version to delete

--- snakemake/install.sh
+++ snakemake/install.sh
@@ -1,19 +1,25 @@
-# install anaconda
-https://docs.anaconda.com/anaconda/install/
+# currently, only Linux system tested
+# if use assembly mode, need to have 160GB RAM, otherwise, 34GB is enough
 
-# create conda env called osr
+# install anaconda
+# Please follow instructions from https://docs.anaconda.com/anaconda/install/
+
+echo '>>> cleanup old anaconda packages, if any'
 conda clean --all
+
+echo '>>> create conda env called osr'
 conda env create -n osr -f workflow/envs/osr.yaml
 
-# activate osr
+echo '>>> activate osr'
 conda activate osr
 
-# install R packages into R in osr
-# for DESeq2 
+echo '>>> install R packages into R in osr'
+echo '>> for DESeq2'
 Rscript -e "install.packages( c('BiocManager', 'ggrepel', 'gdata', 'plyr', 'pheatmap', 'PoiClaClu', 'gridExtra', 'rmarkdown', 'ashr'), repos='https://cloud.r-project.org')"
 Rscript -e "BiocManager::install('scater')"
-# for QoRTs
+echo '>> for QoRTs'
 Rscript -e 'install.packages("http://hartleys.github.io/QoRTs/QoRTs_STABLE.tar.gz", repos=NULL, type="source");'
+echo '>> for DEXSeq'
 pip install HTSeq
 
 ### test run ### 
