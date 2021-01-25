c09fcc0abdb7e026ee7cc8c528eef14adcc1f268
radio1988 <liruiradiant@gmail.com>
2021-01-25 12:07:31 -0500

    updated

--- snakemake/workflow/script/for_dev_only/submit.sh
+++ snakemake/workflow/script/for_dev_only/submit.sh
@@ -4,22 +4,19 @@
 # OR: bsub -q long -W 144:00 -R rusage[mem=4000] 'bash submit.sh'
 
 module purge
-module load singularity/singularity-current > nohup.out   2>&1 
 source activate osr >> nohup.out  2>&1 
 
 # fast jobs with short queue
-snakemake -p -k --jobs 16 \
---use-envmodules \
+snakemake -p -k --jobs 99 \
 --latency-wait 120 \
---ri --restart-times 1 \
+--ri --restart-times 0 \
 --cluster 'bsub -q short -o lsf.log -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 4:00' >> nohup.out  2>&1 
 
 # slower jobs with long queue
 # low RAM jobs ran 3 times with triple RAM
 snakemake -j 1 --unlock
-snakemake -p -k --jobs 16 \
---use-envmodules \
---latency-wait 300 \
+snakemake -p -k --jobs 99 \
+--latency-wait 120 \
 --ri --restart-times 2 \
 --cluster 'bsub -q long -o lsf.log -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 48:00' >> nohup.out  2>&1 
 
