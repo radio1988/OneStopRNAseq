41542b902d33b586f86302aa2b28b33f4ac0f085
radio1988 <liruiradiant@gmail.com>
2021-01-25 11:57:27 -0500

    v2

--- snakemake/workflow/script/for_dev_only/submit.webserver.sh
+++ snakemake/workflow/script/for_dev_only/submit.webserver.sh
@@ -1,19 +1,18 @@
-cd /home/kh45w/project/umw_hira_goel/OneStopRNAseq/users/89/Example_study1_10.10.21-01.25.2021/analysis_1
+cd $analysis_path
 
 module purge
-module load singularity/singularity-current > nohup.out 2>&1
 source /home/kh45w/src/anaconda3/etc/profile.d/conda.sh
 conda activate /home/kh45w/umw_mccb/Kai/src/conda_env/osr_test >> nohup.out 2>&1
-snakemake -j 1 --unlock
-snakemake -p -k --jobs 16 --use-singularity --use-envmodules --use-conda --conda-prefix '/project/umw_mccb/OneStopRNAseq/conda/' --ri --restart-times 0 --latency-wait 300 --cluster 'bsub -q short -o lsf.log -R 'rusage[mem={resources.mem_mb}]' -n {threads} -R span[hosts=1] -W 4:00' > log.snakemake.txt 2>&1
 
 snakemake -j 1 --unlock
-snakemake -p -k --jobs 16 --use-singularity --use-envmodules --use-conda --conda-prefix '/project/umw_mccb/OneStopRNAseq/conda/' --ri --restart-times 0 --latency-wait 300 --cluster 'bsub -q long -o lsf.log -R 'rusage[mem={resources.mem_mb}]' -n {threads} -R span[hosts=1] -W 48:00' > log.snakemake.txt 2>&1
+snakemake -p -k --jobs 16 --ri --restart-times 0 --latency-wait 120 --cluster 'bsub -q short -o lsf.log -R 'rusage[mem={resources.mem_mb}]' -n {threads} -R span[hosts=1] -W 4:00' > log.snakemake.1.txt 2>&1
 
 snakemake -j 1 --unlock
-snakemake -p -k --jobs 16 --use-singularity --use-envmodules --use-conda --conda-prefix '/project/umw_mccb/OneStopRNAseq/conda/' --ri --restart-times 2 --latency-wait 300 --cluster 'bsub -q long -o lsf.log -R 'rusage[mem={resources.mem_mb}]' -n {threads} -R span[hosts=1] -W 48:00' > log.snakemake.txt 2>&1
+snakemake -p -k --jobs 16 --ri --restart-times 0 --latency-wait 120 --cluster 'bsub -q short -o lsf.log -R 'rusage[mem={resources.mem_mb}]' -n {threads} -R span[hosts=1] -W 4:00' > log.snakemake.2.txt 2>&1
+
+snakemake -j 1 --unlock
+snakemake -p -k --jobs 16 --ri --restart-times 2 --latency-wait 120 --cluster 'bsub -q long -o lsf.log -R 'rusage[mem={resources.mem_mb}]' -n {threads} -R span[hosts=1] -W 48:00' > log.snakemake.3.txt 2>&1
 
 snakemake -j 1 --report report.html > report.log 2>&1
 touch /home/kh45w/project/umw_hira_goel/OneStopRNAseq/users/89/Example_study1_10.10.21-01.25.2021/log_ssh2/analysis1.txt
 python /home/kh45w/umw_mccb/OneStopRNAseq/kai/script/logSnakemakeParser.py log.snakemake.txt > job_status.log.txt
-
