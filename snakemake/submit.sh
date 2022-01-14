b833a3c7925cc05f6af073a49410be483f6744be
radio1988 <liruiradiant@gmail.com>
2022-01-14 15:47:32 -0500

    optional config:blackSamples for DESeq2 step only added

--- snakemake/submit.sh
+++ snakemake/submit.sh
@@ -18,6 +18,7 @@ snakemake -j 1 --report report.html > report.log  2>&1
 snakemake -j 1 --unlock
 
 snakemake -p -k --jobs 99 \
+--use-conda --conda-prefix ~/anaconda3/envs \
 --latency-wait 120 --ri --restart-times 2 \
 --cluster 'bsub -q long -o lsf.log -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -R select[rh=8] -W 140:00' >> workflow.log  2>&1 
 
