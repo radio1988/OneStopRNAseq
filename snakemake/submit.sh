8540ffa40a889ce55017d033522ff202ac333dbe
radio1988 <liruiradiant@gmail.com>
2022-03-15 14:21:44 -0400

    submit.sh: direct to long, just one submission

--- snakemake/submit.sh
+++ snakemake/submit.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-# bsub -q long -W 144:00 -R rusage[mem=4000] 'bash submit.sh'
+# bsub -q long -W 144:00 -R rusage[mem=4000]  -R select[rh=8] 'bash submit.sh'
 
 module purge
 rm -f lsf.log
 source activate osr-base > workflow.log  2>&1 
-
-# first pass with short queue
-snakemake -p -k --jobs 99 \
---use-conda --conda-prefix ~/anaconda3/envs \
---latency-wait 120 --ri --restart-times 0 \
---cluster 'bsub -q short -o lsf.log -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -R select[rh=8] -W 4:00' >> workflow.log  2>&1 
-
-snakemake -j 1 --report report.html > report.log  2>&1
-
-# slower jobs with long queue
-# low RAM jobs ran 3 times with triple RAM
-snakemake -j 1 --unlock
 
 snakemake -p -k --jobs 99 \
 --use-conda --conda-prefix ~/anaconda3/envs \
