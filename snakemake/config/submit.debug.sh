4d26df10e5651f04790e4f596354ffb59bae845e
rui <liruiradiant@gmail.com>
2023-03-13 16:21:03 -0400

    cleaned up osr-base.yaml

--- snakemake/config/submit.debug.sh
+++ snakemake/config/submit.debug.sh
@@ -5,7 +5,7 @@ rm -f lsf.log
 source activate osr-base > workflow.log  2>&1
 
 snakemake -p -k --jobs 99 \
---use-conda --conda-prefix ~/miniconda3/envs/osr_envs \
+--use-conda --conda-prefix ~/anaconda3/envs/osr_envs \
 --latency-wait 120 --ri --restart-times 0 \
 --cluster 'bsub -q long -o lsf.log -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 140:00' \
 --cluster-cancel bkill \
