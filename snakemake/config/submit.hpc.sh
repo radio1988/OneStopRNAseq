7317dc03c1b1b5d35136671957b24611641f7c14
rui <liruiradiant@gmail.com>
2023-11-02 14:24:30 -0400

    submit code updated

--- snakemake/config/submit.hpc.sh
+++ snakemake/config/submit.hpc.sh
@@ -12,4 +12,14 @@ snakemake -p -k --jobs 99 \
 --cluster-cancel bkill \
 >> workflow.log  2>&1 
 
+
+snakemake -p -k --jobs 99 \
+--use-conda --conda-prefix ~/anaconda3/envs/osr_envs \
+--latency-wait 120 --ri --restart-times 1 \
+--rerun-triggers mtime \
+--cluster 'bsub -q long -o lsf.log -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 140:00' \
+--cluster-cancel bkill \
+>> workflow.log  2>&1 
+
+
 snakemake -j 1 --report report.html > report.log  2>&1
