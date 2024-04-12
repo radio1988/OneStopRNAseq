db61529c7207ab20c43f614597ddbf981c15dc3d
rui <liruiradiant@gmail.com>
2024-04-12 10:24:51 -0400

    T2T anno_tab included chr info

--- snakemake/workflow/script/gtf2anno_tab.py
+++ snakemake/workflow/script/gtf2anno_tab.py
@@ -7,6 +7,7 @@ print("Input: {}\nOutput: {}".format(file,outfile))
 
 
 o = open (outfile,'w')
+o.write("\t".join(['Gene','Name','Type','Chr','Strand','Start','End'])+"\n")
 
 with open(file) as f:
     while True:
@@ -24,12 +25,14 @@ with open(file) as f:
         if feature != "gene":
             continue
     
-        
-        gene_name = re.search(r'gene_name "([^\"]+)"', attribute).groups()[0]
         gene_id = re.search(r'gene_id "([^\"]+)"', attribute).groups()[0]
-        gene_type = re.search(r'gene_type "([^\"]+)"', attribute).groups()[0]
+        if re.search(r'gene_name "([^\"]+)"', attribute): 
+            gene_name = re.search(r'gene_name "([^\"]+)"', attribute).groups()[0]
+        else:
+            gene_name = gene_id
+        gene_type = re.search(r'gene_biotype "([^\"]+)"', attribute).groups()[0]
 
-        outline = "\t".join([gene_id,gene_name,gene_type,seqname,start,end])+"\n"
+        outline = "\t".join([gene_id,gene_name,gene_type,seqname,strand,start,end])+"\n"
         o.write(outline)
 
 o.close()
