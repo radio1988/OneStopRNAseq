b4d6341613283ee683f79cc2e707c4ac9e75832d
rui <liruiradiant@gmail.com>
2024-04-19 00:24:37 -0400

    submit.debug.sh: find conda on hpc

--- snakemake/config/submit.debug.sh
+++ snakemake/config/submit.debug.sh
@@ -2,7 +2,8 @@
 # bsub -q long -W 144:00 -R rusage[mem=4000]  'bash submit.debug.sh'
 
 rm -f lsf.log
-source activate osr-base > workflow.log  2>&1
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate osr-base > workflow.log  2>&1
 
 snakemake -pk --jobs 99 --rerun-triggers mtime \
 --use-conda --conda-prefix ~/anaconda3/envs/osr_envs \
