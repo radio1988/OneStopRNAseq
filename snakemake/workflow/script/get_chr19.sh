6b654b38eb09f917be5a4e1d101527d90b4bd792
rui <liruiradiant@gmail.com>
2024-05-03 10:58:28 -0400

    get_chr19.sh for example data

---
+++ snakemake/workflow/script/get_chr19.sh
@@ -0,0 +1,13 @@
+mkdir -p chr19 mkdir fastq
+
+#conda activate bio # samtools, bioconda::bedtools
+# workdir: where bam are located
+# outputdir: ./chr19 ./fastq
+# function: get chr19 from bam, export R1 R2 as fastq.gz, for example-test data
+# why chr19: shortest in mm10, pretty short in hs38 too
+
+for f in *bam
+do 
+samtools view -h -@ 4 $f chr19 | samtools sort -n -@ 4 -o chr19/$f 
+bamToFastq -i chr19/$f -fq fastq/${f/bam/R1.fastq.gz} -fq2 fastq/${f/bam/R2.fastq.gz}
+done
