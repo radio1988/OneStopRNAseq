304b9860c703edb7bf7967bf7937ef6368757772
rui <liruiradiant@gmail.com>
2024-05-10 16:15:56 -0400

    targets: create sqlite from gtf, real test

--- snakemake/workflow/script/make_ensdb.R
+++ snakemake/workflow/script/make_ensdb.R
@@ -1,17 +1,14 @@
 log <- file(snakemake@log[[1]], open="wt")
 sink(log)
 
-#library(CleanUpRNAseq)
-file.create(snakemake@output[['ensdb']])
+library(CleanUpRNAseq)
 
-
-
-# make_ensdb(
-#   gtf=snakemake@input[['gtf']],
-#   outfile = snakemake@output[['ensdb']]
-#   organism_latin_name = "NA",
-#   genome_version = "NA",
-#   Ensembl_release_version = "NA"
-# )
+make_ensdb(
+  gtf=snakemake@input[['gtf']],
+  outfile = snakemake@output[['ensdb']]
+  organism_latin_name = "NA",
+  genome_version = "NA",
+  Ensembl_release_version = "NA"
+)
 
 #todo: correct version numbers
