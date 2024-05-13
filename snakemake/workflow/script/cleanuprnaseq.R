47de576cdde2ad36cd0ed2ae4122bc2c11364d22
rui <liruiradiant@gmail.com>
2024-05-13 13:27:05 -0400

    CleanUpRNAseq sink type

--- snakemake/workflow/script/cleanuprnaseq.R
+++ snakemake/workflow/script/cleanuprnaseq.R
@@ -2,19 +2,15 @@ if (!requireNamespace("pheatmap", quietly = TRUE)) {
   install.packages("pheatmap", repos='http://cran.us.r-project.org')
 }
 
-if (!requireNamespace("readJDX", quietly = TRUE)) {
-  install.packages("readJDX", repos='http://cran.us.r-project.org')
-}
-
 if (!requireNamespace("CleanUpRNAseq", quietly = TRUE)) {
 install.packages('./workflow/envs/CleanUpRNAseq/', repos = NULL, type="source")  # works
 }
 
 library(CleanUpRNAseq)
-library(readJDX)
 
 log <- file(snakemake@log[[1]], open="wt")
-sinkall(log)
+sink(log, type = c("output", "message"))
+
 message("Working Directory:", getwd())
 
 meta <- read.csv(snakemake@input[['meta']])
