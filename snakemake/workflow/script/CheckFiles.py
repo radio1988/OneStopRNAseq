82caea48b6b13a9ec8c5101ffeface774b263639
rui <liruiradiant@gmail.com>
2024-07-16 14:56:44 -0400

    CheckFiles.py

--- snakemake/workflow/script/CheckFiles.py
+++ snakemake/workflow/script/CheckFiles.py
@@ -18,7 +18,7 @@ output = open(snakemake.output[0], "w")
 
 file_list = snakemake.input
 print(file_list, file=output)
-if are_files_non_empty(file_list):
+if are_files_too_small(file_list):
     print("All trimmed.fastq.gz files are big enough", file=output)
 else:
     print("At least one trimmed fastq.gz file is too small", file=output)
