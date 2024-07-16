9f1fed27ca47e195d9c32ff5b70ebf879f17f1d6
rui <liruiradiant@gmail.com>
2024-07-16 18:02:36 -0400

    log added for failed job

--- snakemake/workflow/script/CheckFileSizes.py
+++ snakemake/workflow/script/CheckFileSizes.py
@@ -1,35 +1,24 @@
-import os
-import sys
+import os, sys
 
 output = open(snakemake.output[0], "w")
 log = open(snakemake.log[0], "w")
 
+filenames = snakemake.input
 
-def count_of_small_files(filenames):
-    """
-    Input: list of filenames
-    Return: count of small files less than 100 in size (int)
-    """
-    N = 0
-    for filename in filenames:
-        m = filename + str(os.path.getsize(filename))
-        print(m, file=output)
-        if os.path.getsize(filename) < 100:
-            print("file too small: " + m, file=log)
-            N += 1
-    return N
-
-
-file_list = snakemake.input
-
-N = count_of_small_files(file_list)
+N = 0
+for filename in filenames:
+    message = filename + str(os.path.getsize(filename))
+    print(message, file=output)
+    if os.path.getsize(filename) < 100:
+        print("file too small: " + message, file=log)
+        N += 1
 
 if N < 1:
     print("All files are big enough", file=output)
 else:
     outstring = "{} files too small, workflow aborted!!!".format(N)
-    sys.stderr.write(outstring + "\n\n")
     print(outstring, file=output)
+    print(outstring, file=log)
     sys.exit(outstring)
 
 output.close()
