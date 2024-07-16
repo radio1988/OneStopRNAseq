734722fbc3d8f301fde836bda4f86833d5290bfe
rui <liruiradiant@gmail.com>
2024-07-16 14:34:25 -0400

    755

--- snakemake/workflow/example_data/commands/submit.hpc.sh
+++ snakemake/workflow/example_data/commands/submit.hpc.sh
@@ -8,14 +8,14 @@ mkdir -p log/lsf/
 snakemake -p -k --jobs 99 \
 --use-conda --conda-prefix ~/anaconda3/envs/osr_envs \
 --ri --restart-times 0 --rerun-triggers mtime \
---cluster 'bsub -q long -o log/lsf/%J.lsf.txt -e log/lsf/%J.snakemake.txt -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 140:00' \
+--cluster 'bsub -q long -N -o log/lsf/%J.lsf.txt -e log/lsf/%J.snakemake.txt -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 140:00' \
 >> workflow.log  2>&1 
 
 
 snakemake -p -k --jobs 99 \
 --use-conda --conda-prefix ~/anaconda3/envs/osr_envs \
 --ri --restart-times 2 --rerun-triggers mtime \
---cluster 'bsub -q long -o log/lsf/%J.lsf.txt -e log/lsf/%J.snakemake.txt -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 140:00' \
+--cluster 'bsub -q long -N -o log/lsf/%J.lsf.txt -e log/lsf/%J.snakemake.txt -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 140:00' \
 --cluster-cancel bkill \
 >> workflow.log  2>&1 
 
