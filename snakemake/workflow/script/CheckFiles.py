a48e48e1791db614a2222425752c89ce906f2a32
rui <liruiradiant@gmail.com>
2024-07-16 15:01:24 -0400

    CheckFiles.py

--- snakemake/workflow/script/CheckFiles.py
+++ snakemake/workflow/script/CheckFiles.py
@@ -1,27 +1,28 @@
 import os
 import sys
 
-
-def are_files_too_small(filenames):
+output = open(snakemake.output[0], "w")
+def count_of_small_files(filenames):
     """
-    if one file or more is too small, return False
+    Input: list of filenames
+    Return: count of small files less than 100 in size (int)
     """
-    Flag = True
+    N = 0
     for filename in filenames:
         print(filename, os.path.getsize(filename), file=output)
         if os.path.getsize(filename) < 100:
-            Flag = False
-    return Flag
+            N += 1
+    return N
 
-
-output = open(snakemake.output[0], "w")
 
 file_list = snakemake.input
 print(file_list, file=output)
-if are_files_too_small(file_list):
+N = count_of_small_files(file_list)
+if N < 1:
     print("All trimmed.fastq.gz files are big enough", file=output)
 else:
-    print("At least one trimmed fastq.gz file is too small", file=output)
-    sys.exit("At least one trimmed fastq.gz file is too small")
+    outstring = "{} trimmed.fastq.gz files too small, workflow aborted".format(N)
+    print(outstring, file=output)
+    sys.exit(outstring)
 
 output.close()
