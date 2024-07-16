724e097525a5a13a178a8d002822813f6f295bb5
rui <liruiradiant@gmail.com>
2024-07-16 14:55:59 -0400

    CheckFiles.py

--- snakemake/workflow/script/CheckFiles.py
+++ snakemake/workflow/script/CheckFiles.py
@@ -2,31 +2,26 @@ import os
 import sys
 
 
-def are_files_non_empty(filenames):
-  """Checks if all files in the given list are non-empty.
+def are_files_too_small(filenames):
+    """
+    if one file or more is too small, return False
+    """
+    Flag = True
+    for filename in filenames:
+        print(filename, os.path.getsize(filename), file=output)
+        if os.path.getsize(filename) < 100:
+            Flag = False
+    return Flag
 
-  Args:
-    filenames: A list of file paths.
 
-  Returns:
-    True if all files are non-empty, False otherwise.
-  """
-
-  for filename in filenames:
-    print(filename, os.path.getsize(filename), file=output)
-    if os.path.getsize(filename) < 100:
-      return False
-  return True
-
-# Example usage:
 output = open(snakemake.output[0], "w")
 
 file_list = snakemake.input
 print(file_list, file=output)
 if are_files_non_empty(file_list):
-  print("All files are non-empty", file= output)
+    print("All trimmed.fastq.gz files are big enough", file=output)
 else:
-  print("At least one file is empty", file=output)
-  sys.exit("At least trimmed fastq.gz is too small")
+    print("At least one trimmed fastq.gz is too small", file=output)
+    sys.exit("At least one trimmed fastq.gz is too small")
 
 output.close()
