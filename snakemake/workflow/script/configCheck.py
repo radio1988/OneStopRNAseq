44cbe4d2a43641edd9a3651599e25eb8239931af
rui <liruiradiant@gmail.com>
2024-07-17 16:05:49 -0400

    INTRON and gDNA correction no longer compatible

---
+++ snakemake/workflow/script/configCheck.py
@@ -0,0 +1,21 @@
+import sys
+
+output = open(snakemake.output[0], "w")
+log = open(snakemake.log[0], "w")
+
+
+# INTRON and gDNA correction, only can choose one at most
+if snakemake.config["INTRON"] and snakemake.config["CleanUpRNAseqCorrection"]:
+    message = "INTRON mode and CleanUpRNAseqCorrection is incompatible. \
+    gDNA correction is only possible for exon level rnaseq quantification"
+    print(message, file=log)
+    sys.exit(message)
+else:
+    print("INTRON {} and CleanUpRNAseqCorrection {} compatible".format(
+        snakemake.config['INTRON'], snakemake.config['CleanUpRNAseqCorrection'],
+        file = output
+    ))
+
+
+# SPECIES and Analysis options
+# Not now
