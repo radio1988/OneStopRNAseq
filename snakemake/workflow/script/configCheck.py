3d918da97fb0d018725baef0fde577364679173e
rui <liruiradiant@gmail.com>
2024-07-18 12:41:55 -0400

    grammar

--- snakemake/workflow/script/configCheck.py
+++ snakemake/workflow/script/configCheck.py
@@ -6,7 +6,7 @@ log = open(snakemake.log[0], "w")
 
 # INTRON and gDNA correction, only can choose one at most
 if snakemake.config['START'] == 'FASTQ' and snakemake.config["INTRON"] and snakemake.config["CleanUpRNAseqCorrection"]:
-    message = "INTRON mode and CleanUpRNAseqCorrection is incompatible. \
+    message = "INTRON mode and CleanUpRNAseqCorrection incompatible. \
     gDNA correction is only possible for exon level rnaseq quantification"
     print(message, file=log)
     sys.exit(message)
