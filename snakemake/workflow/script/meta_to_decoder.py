f97df4e4f6681a624a98ac023e5a66b0ef1e7eca
rui <liruiradiant@gmail.com>
2024-07-17 21:40:18 -0400

    read_table

--- snakemake/workflow/script/meta_to_decoder.py
+++ snakemake/workflow/script/meta_to_decoder.py
@@ -1,7 +1,24 @@
-import pandas as pd  
+import pandas as pd
 import sys
-from osr import read_table
+
+
+def read_table(fname='meta/contrast.de.xlsx'):
+    try:
+        if fname.endswith(".txt"):
+            df = pd.read_table(fname)
+        elif fname.endswith(".csv"):
+            df = pd.read_csv(fname)
+        elif fname.endswith(".xlsx"):
+            df = pd.read_excel(fname, engine='openpyxl')
+        else:
+            sys.exit("fname not xlsx nor txt")
+    except:
+        sys.exit(">>> Fatal Error: " + fname + " format error, it can't be read correctly." +
+                 "\nPlease check if you saved txt file as xlsx or vice versa\n\n")
+    return (df)
+
+
 fname = sys.argv[1]
-df=read_table(fname)
-df.columns = ["sample.ID", "group.ID", "batch.ID"] 
-df.to_csv("meta/decoder.txt", sep="\t", index=False)  
+df = read_table(fname)
+df.columns = ["sample.ID", "group.ID", "batch.ID"]
+df.to_csv("meta/decoder.txt", sep="\t", index=False)
