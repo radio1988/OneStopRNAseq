fcb9257bdc7eac3b00c61beef34457f913a771cf
rui <liruiradiant@gmail.com>
2025-02-14 11:22:13 -0500

    gsea_bubble plot from nrows * 0.17 + 1 to nrows * 0.1 + 3

--- snakemake/workflow/script/gsea_bubble.py
+++ snakemake/workflow/script/gsea_bubble.py
@@ -106,7 +106,7 @@ def create_bubble_plot(df, output_path="folder/plot.pdf", alpha='alpha'):
     # Create bubble plot
     nrows = df['GeneSet'].shape[0]
 
-    plt.figure(figsize=(10, nrows * 0.17 + 1))
+    plt.figure(figsize=(10, nrows * 0.1 + 3))
 
     if nrows < 1:
         plt.text(
